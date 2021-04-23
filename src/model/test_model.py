92b9024c3c3b44411f3d3225bee6b66adf5df0d3
KyoungYeulLee <ney8909@gmail.com>
2021-04-23 16:02:01 +0900

    final code

--- src/model/test_model.py
+++ src/model/test_model.py
@@ -1,8 +1,7 @@
 '''
 A module to test the model and get performances
-python test_model.py -n (model_num) (-c config.yaml)
 '''
-
+import os
 import argparse
 import logging
 import yaml
@@ -17,17 +16,27 @@ class SingleTester():
     '''
     def __init__(self, config: dict):
         self.config = config
+        self.data_dir = config['DATA_DIR']
         self.model_num = config['VALID']['MODEL_NUM'][0]
         self.test_metrics = config['VALID']['VALID_METRICS']
-        self.log_dir = f"{config['MODEL']['MODEL_DIR']}/{config['MODEL']['MODEL_TYPE']}/" +\
-                    config['MODEL']['MODEL_NAME']
+        self.log_dir = os.path.join(
+            self.data_dir,
+            config['MODEL']['MODEL_DIR'],
+            config['MODEL']['MODEL_TYPE'],
+            config['MODEL']['MODEL_NAME'],
+        )
         self.model = None
 
-        self.ext_dataset, self.ext_mut = \
-            tr.get_dataset(config['VALID']['VALID_DATA_PATH'], config['VALID']['VALID_ID_PATH'])
+        self.ext_dataset, self.ext_mut = tr.get_dataset(
+            os.path.join(self.data_dir, config['VALID']['VALID_DATA_PATH']),
+            os.path.join(self.data_dir, config['VALID']['VALID_ID_PATH']),
+        )
 
         if tr.get_data_format(config['MODEL']['MODEL_TYPE']) == 'rapsix':
-            self.ext_dataset = tr.attach_snvbox(self.ext_dataset, config['VALID']['VALID_SNVBOX'])
+            self.ext_dataset = tr.attach_snvbox(
+                self.ext_dataset,
+                os.path.join(self.data_dir, config['VALID']['VALID_SNVBOX']),
+            )
 
         self.ext_dataset, self.ext_mut = tr.remove_vus(self.ext_dataset, self.ext_mut)
 
@@ -45,7 +54,9 @@ class SingleTester():
         '''
         config = self.config
         if config['VALID']['USE_TEST_ID'][0]: # when using samples of test_ids among the dataset
-            test_ids = np.load(f"{config['log_dir']}/test_ids.npy")
+            test_ids = np.load(
+                os.path.join(config['log_dir'], 'test_ids.npy')
+            )
             ext_idx = np.array(range(len(self.ext_mut)))
             _, test_idx = tr.split_sample(ext_idx, mut_ids=self.ext_mut, test_mut=test_ids)
             test_set = tr.data_subset(self.ext_dataset, test_idx)
@@ -64,8 +75,19 @@ class SingleTester():
         valid_name = config['VALID']['VALID_NAME']
         self.model.load(model_idx)
         test_pred = self.model.predict(test_set)
-        np.save(f"{config['log_dir']}/{valid_name}_{model_idx}.npy", test_pred)
-        with open(f"{config['log_dir']}/{valid_name}_{model_idx}_ours.txt", 'w') as score_file:
+        np.save(
+            os.path.join(
+                config['log_dir'],
+                f"{valid_name}_{model_idx}.npy"
+            ),
+            test_pred
+        )
+        with open(
+            os.path.join(
+                config['log_dir'],
+                f"{valid_name}_{model_idx}_ours.txt"
+            ), 'w'
+        ) as score_file:
             for mut, ans, pred in zip(test_ids, test_set[2][:, 0], test_pred[:, 0]):
                 score_file.write(f"{mut},{ans},{pred}\n")
         return test_pred
@@ -78,8 +100,15 @@ class SingleTester():
         result_list = []
         valid_name = self.config['VALID']['VALID_NAME']
         if test_metrics: # skip when test_metrics == []
-            result_list = self.model.evaluate(test_set[2], test_pred, metrics=test_metrics, \
-                out_path=f"{self.config['log_dir']}/{valid_name}_{model_idx}.txt")
+            result_list = self.model.evaluate(
+                test_set[2],
+                test_pred,
+                metrics=test_metrics,
+                out_path=os.path.join(
+                    self.config['log_dir'],
+                    f"{valid_name}_{model_idx}.txt",
+                )
+            )
         return result_list
 
     def test_model(self):
@@ -110,7 +139,7 @@ def main():
     parser.add_argument('-c', '--config', default='config.yaml', help='path for config file')
     parser.add_argument('-t', '--title', default='code', help='title of this job')
     args = parser.parse_args()
-    logging.basicConfig(filename='test_model.log',
+    logging.basicConfig(filename='test_model.py.log',
                        format='%(asctime)s %(levelname)-8s %(message)s',
                        level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
     with open(args.config) as yml:
